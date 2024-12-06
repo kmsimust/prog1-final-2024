800090bbcd07d0af29aaa370c4236832c8b6d86a
Phubet Ueananta <Phubet.u@ku.th>
2024-12-06 11:56:02 +0700

    fix commit2

--- ball.py
+++ ball.py
@@ -1,32 +1,47 @@
 import turtle
 
-def draw_ball(color, size, x, y):
+class Ball:
+    def __init__(self,
+                 size,
+                 x,
+                 y,
+                 vx,
+                 vy,
+                 color):
+        self.size = size
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.color = color
+        self.canvas_width = turtle.screensize()[0]
+        self.canvas_height = turtle.screensize()[1]
+
+
+    def draw_ball(self):
     # draw a circle of radius equals to size at x, y coordinates and paint it with color
-    turtle.penup()
-    turtle.color(color)
-    turtle.fillcolor(color)
-    turtle.goto(x,y-size)
-    turtle.pendown()
-    turtle.begin_fill()
-    turtle.circle(size)
-    turtle.end_fill()
+        turtle.penup()
+        turtle.color(self.color)
+        turtle.fillcolor(self.color)
+        turtle.goto(self.x, self.y - self.size)
+        turtle.pendown()
+        turtle.begin_fill()
+        turtle.circle(self.size)
+        turtle.end_fill()
+        turtle.penup()
 
-def move_ball(i, xpos, ypos, vx, vy, dt):
+
+    def move_ball(self, dt):
     # update the x, y coordinates of ball i with velocity in the x (vx) and y (vy) components
-    xpos[i] += vx[i]*dt
-    ypos[i] += vy[i]*dt
+        self.x += self.vx*dt
+        self.y += self.vy*dt
 
 
-def update_ball_velocity(i, xpos, ypos, vx, vy, canvas_width, canvas_height, ball_radius):
+    def update_ball_velocity(self):
     # if the ball hits the side walls, reverse the vx velocity
-    if abs(xpos[i]) > (canvas_width - ball_radius):
-        vx[i] = -vx[i]
+        if abs(self.x) > (self.canvas_width - self.size):
+            self.vx = -self.vx
 
     # if the ball hits the ceiling or the floor, reverse the vy velocity
-    if abs(ypos[i]) > (canvas_height - ball_radius):
-        vy[i] = -vy[i]
-
-
-
-
-
+        if abs(self.y) > (self.canvas_height - self.size):
+            self.vy = -self.vy
